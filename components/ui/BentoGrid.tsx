9d24aa1daec10f33947f2ac6db1d116edd317b2a
Aryan-219 <aryanagr219@gmail.com>
2024-08-07 23:25:37 +0530

    second commit

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["TAILWIND", "JAVA EE", "JAVASCRIPT"];
+  const rightLists = ["REACT", "MYSQL", "C++"];
 
   const [copied, setCopied] = useState(false);
 
